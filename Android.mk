218479f46c03a7d41b0e626322c9f077ea431d00
Alexander Martinz <amartinz@shiftphones.com>
2019-03-27 12:29:57 +0100

    Camera2: initial gradle support
    Change-Id: I7271b99dbdf4104e223d555179564de79930ed45
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- Android.mk
+++ Android.mk
@@ -42,4 +42,30 @@ LOCAL_JNI_SHARED_LIBRARIES := libjni_tinyplanet libjni_jpegutil
 
 include $(BUILD_PACKAGE)
 
+### FAKE RULES FOR ANDROID STUDIO SUPPORT
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GenerateStudioFiles-Camera2
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+camera2_system_deps_framework := $(call java-lib-deps,framework)
+
+camera2_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+.PHONY: copy_camera2_system_deps
+copy_camera2_system_deps: $(camera2_system_deps_framework)
+	$(hide) mkdir -p $(camera2_system_libs_path)
+	$(hide) rm -rf $(camera2_system_libs_path)/*.jar
+	$(hide) cp $(camera2_system_deps_framework) $(camera2_system_libs_path)/framework.jar
+
+$(LOCAL_BUILT_MODULE): copy_camera2_system_deps
+	$(hide) echo "Fake: $@"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) touch $@
+
+### FAKE RULES FOR ANDROID STUDIO SUPPORT END
+
 include $(call all-makefiles-under, $(LOCAL_PATH))

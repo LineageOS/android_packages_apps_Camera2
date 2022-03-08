c8f7b2f3b426f264327e8f70bcd3ea1cf4d7c941
Emilian Peev <epeev@google.com>
2022-03-08 11:40:33 -0800

    Remove Help & Feedback option from filmstrip
    The "Help & Feedback" menu option in the camera
    filmstrip context menu is not functional and can
    be confusing to users.
    
    Bug: 169469748
    Test: Manual using camera application
    Change-Id: Ibb6bd52e5f813f734eb83dd0ba18394e44cb3538

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1371,10 +1371,6 @@ public class CameraActivity extends QuickActivity
             case R.id.action_details:
                 showDetailsDialog(mFilmstripController.getCurrentAdapterIndex());
                 return true;
-            case R.id.action_help_and_feedback:
-                mResetToPreviewOnResume = false;
-                new GoogleHelpHelper(this).launchGoogleHelp();
-                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -2368,17 +2364,6 @@ public class CameraActivity extends QuickActivity
         }
 
         return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        if (isSecureCamera() && !ApiHelper.isLOrHigher()) {
-            // Compatibility pre-L: launching new activities right above
-            // lockscreen does not reliably work, only show help if not secure
-            menu.removeItem(R.id.action_help_and_feedback);
-        }
-
-        return super.onPrepareOptionsMenu(menu);
     }
 
     protected long getStorageSpaceBytes() {

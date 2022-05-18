22c66321750c9f291e8495afc4fc635c2edc0443
joey <joey@lineageos.org>
2022-05-18 11:16:18 +0200

    Camera2: add QR scanner QS tile
    Signed-off-by: joey <joey@lineageos.org>
    Change-Id: Ide4944e8f682661a6bb2a650b8c20516e0dc080b

---
+++ quickReader/src/org/lineageos/quickreader/ScannerTileService.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.quickreader;
+
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import com.android.camera2.R;
+
+public final class ScannerTileService extends TileService {
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        startActivityAndCollapse(new Intent(this, ScannerActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+}
